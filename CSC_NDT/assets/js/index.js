e15c34176a4352b3668f0d03e147a22e5735281a
f1991911 <90699148+f1991911@users.noreply.github.com>
2021-09-14 21:26:41 +0800

    CSC NDT

---
+++ CSC_NDT/assets/js/index.js
@@ -0,0 +1,31 @@
+var viewportWidth, divWidth, tb;
+	$(function() {
+		
+		viewport = $('#container').innerWidth();
+		tb = $('#thumbs');
+		divWidth = tb.outerWidth();
+    
+		$('#container').mousemove(function(e)
+		{
+      tb.css({left: ((viewport - divWidth)*((e.pageX / viewport).toFixed(3))).toFixed(1) +"px" });
+ 		});
+    
+    $('.history-block').on('click', function(){
+      $('.history-block').css('width', '350px');
+      $('.history-block').find('.title').css('width', '300px');
+        $('.history-block').find('.year').css('width', '260px');
+       $('.history-block .timeline').hide(300);
+        $(this).css('width', '600px');
+        $(this).find('.title').css('width', '500px');
+        $(this).find('.year').css('width', '500px');
+       $(this).find('.timeline').show(800);
+      $('#container').mousemove(function(e)
+        {
+          tb.css({left: ((viewport - divWidth-300)*((e.pageX / viewport).toFixed(3))).toFixed(1) + 300 + "px" });
+          });
+    });
+    
+    $('.timeline ul li').on('click', function(){
+        $(this).parent().blink();
+    });
+});
